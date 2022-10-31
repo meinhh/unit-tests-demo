4aaa1c907f0e68f8f0e35f338c6e661ef3b5746b
Meirav Shenhar <meinhh@gmail.com>
2022-10-31 14:50:14 +0200

    step 3 - added test cases for antivirus mock

--- src/anagramFinder.spec.ts
+++ src/anagramFinder.spec.ts
@@ -29,4 +29,23 @@ describe('isAnagram', () => {
 		expect.assertions(1)
 		await expect(anagramFinder.isAnagram('virus', 'test')).rejects.toThrowError('Value is a virus')
 	})
+	describe('should check if values are viruses', () => {
+		it('should throw an exception on string 1 is a virus', async () => {
+			mockedAntiVirus.isVirus = jest.fn().mockResolvedValue(true)
+			expect.assertions(1)
+			await expect(anagramFinder.isAnagram('virus', 'test')).rejects.toThrowError('Value is a virus')
+		})
+		it('should throw an exception on string 2 is a virus', async () => {
+			mockedAntiVirus.isVirus = jest.fn().mockResolvedValue(true)
+			expect.assertions(1)
+			await expect(anagramFinder.isAnagram('test', 'virus')).rejects.toThrowError('Value is a virus')
+		})
+		it('should check for viruses on valid inputs', async () => {
+			const str1 = 'test1', str2 = 'test2'
+			await anagramFinder.isAnagram(str1, str2)
+			expect(mockedAntiVirus.isVirus).toHaveBeenCalledTimes(2)
+			expect(mockedAntiVirus.isVirus).toHaveBeenCalledWith(str1)
+			expect(mockedAntiVirus.isVirus).toHaveBeenCalledWith(str2)
+		})
+	});
 });
